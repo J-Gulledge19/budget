9f5b18151d2082f9ae806f92277ee54639a494b2
Josh Gulledge <jgulledge19@gmail.com>
2022-11-12 16:41:34 -0700

    Fixed styling link and added show/new route

--- server.js
+++ server.js
@@ -12,9 +12,10 @@ app.use(express.urlencoded({extended: true}))
 app.use(methodOverride("_method")) 
 // app.use("/static", express.static("public")) 
 
+app.get("/", (req, res) => res.redirect("/budget"))
 
 // HOME ROUTE
-app.get('/', (req, res) => {
+app.get('/budget', (req, res) => {
     res.render(
         'index.ejs',
         {
@@ -24,10 +25,23 @@ app.get('/', (req, res) => {
     );
 });
 
+// Add New Budget Items
+app.get('/budget/new', (req, res) => {
+    res.render('new.ejs');
+});
+
+// Create Route and redirect the user back to index
+app.post("/budget", (req, res) => {
+   
+    budget.push(req.body);
+  
+    res.redirect("/budget");
+  });
+
 //SHOW ROUTE - Returns a single budget
 app.get("/budget/:index", (req, res) => {
     res.render("show.ejs", {
-      fruit: budget[req.params.index],
+      budget: budget[req.params.index],
       index: req.params.index
     });
   });
